80ef3c4b590e8477740c2502b875b4b7c3752eed
joaoajm@gmail.com <joaoajm@gmail.com>
2017-08-28 23:51:55 -0300

    Autenticação Oauth e basic baseado no profile do spring

--- src/main/java/com/jajm/lancamentosapi/repository/PessoaRepository.java
+++ src/main/java/com/jajm/lancamentosapi/repository/PessoaRepository.java
@@ -1,9 +1,13 @@
 package com.jajm.lancamentosapi.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.jajm.lancamentosapi.model.Pessoa;
 
 public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
+	
+	public Page<Pessoa> findByNomeContaining(String nome, Pageable pageabre);
 
 }
--- src/main/java/com/jajm/lancamentosapi/resource/PessoaResource.java
+++ src/main/java/com/jajm/lancamentosapi/resource/PessoaResource.java
@@ -1,14 +1,15 @@
 package com.jajm.lancamentosapi.resource;
 
-import java.util.List;
-
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -44,11 +46,6 @@ public class PessoaResource {
 		return ResponseEntity.status(HttpStatus.CREATED).body(pessoaSalva);
 	}
 	
-	@GetMapping
-	public List<Pessoa> listarTodos(){
-		return pessoaRepository.findAll();
-	}
-	
 	@GetMapping("/{codigo}")
 	public ResponseEntity<Pessoa> buscarPeloCodigo(@PathVariable Long codigo){
 		Pessoa pessoa = pessoaRepository.findOne(codigo);
@@ -71,6 +68,12 @@ public class PessoaResource {
 	@ResponseStatus(HttpStatus.NO_CONTENT)
 	public void atualizarAtivoOuNao(@PathVariable Long codigo, @RequestBody Boolean ativo){
 		pessoaService.atualizarAtivo(codigo, ativo);
+	}
+	
+	@GetMapping
+	@PreAuthorize("hasAuthority('ROLE_PESQUISAR_PESSOA')")
+	public Page<Pessoa> pesquisar(@RequestParam(required = false, defaultValue = "%") String nome, Pageable pageable) {
+		return pessoaRepository.findByNomeContaining(nome, pageable);
 	}
 
 }
