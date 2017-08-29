5a3fad03e167f7e81af874a05a6a9a8dd8d53bc7
drlingerie <ti3@drling.com.br>
2017-08-29 17:33:58 -0300

    Migrando para PostGres

--- src/main/resources/db/migration/V01__criar_e_registrar_categorias.sql
+++ src/main/resources/db/migration/V01__criar_e_registrar_categorias.sql
@@ -1,7 +1,8 @@
+CREATE SEQUENCE seq_categoria START 1;
 CREATE TABLE categoria (
-	codigo BIGINT(20) PRIMARY KEY AUTO_INCREMENT,
+	codigo BIGINT PRIMARY KEY DEFAULT nextval('seq_categoria'),
 	nome VARCHAR(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+);
 
 INSERT INTO categoria (nome) values ('Lazer');
 INSERT INTO categoria (nome) values ('Alimentação');
--- src/main/resources/db/migration/V04__criar_e_registrar_usuarios_e_permissoes.sql
+++ src/main/resources/db/migration/V04__criar_e_registrar_usuarios_e_permissoes.sql
@@ -1,22 +1,24 @@
+CREATE SEQUENCE seq_usuario START 1;
 CREATE TABLE usuario (
-	codigo BIGINT(20) PRIMARY KEY,
+	codigo BIGINT NOT NULL PRIMARY KEY DEFAULT nextval('seq_usuario'),
 	nome VARCHAR(50) NOT NULL,
 	email VARCHAR(50) NOT NULL,
 	senha VARCHAR(150) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+);
 
+CREATE SEQUENCE seq_permissao START 1;
 CREATE TABLE permissao (
-	codigo BIGINT(20) PRIMARY KEY,
+	codigo BIGINT not null PRIMARY KEY DEFAULT nextval('seq_permissao'),
 	descricao VARCHAR(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+);
 
 CREATE TABLE usuario_permissao (
-	codigo_usuario BIGINT(20) NOT NULL,
-	codigo_permissao BIGINT(20) NOT NULL,
+	codigo_usuario BIGINT NOT NULL,
+	codigo_permissao BIGINT NOT NULL,
 	PRIMARY KEY (codigo_usuario, codigo_permissao),
 	FOREIGN KEY (codigo_usuario) REFERENCES usuario(codigo),
 	FOREIGN KEY (codigo_permissao) REFERENCES permissao(codigo)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+);
 
 INSERT INTO usuario (codigo, nome, email, senha) values (1, 'Administrador', 'admin@lancamentosapi.com', '$2a$10$X607ZPhQ4EgGNaYKt3n4SONjIv9zc.VMWdEuhCuba7oLAL5IvcL5.');
 INSERT INTO usuario (codigo, nome, email, senha) values (2, 'Maria Silva', 'maria@lancamentosapi.com', '$2a$10$Zc3w6HyuPOPXamaMhh.PQOXvDnEsadztbfi6/RyZWJDzimE8WQjaq');
